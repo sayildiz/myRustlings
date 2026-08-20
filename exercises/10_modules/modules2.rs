070ce19ba6e997e9e31adcb37fa94f6858081353
samet <samet@framework.home.arpa>
2026-08-20 03:10:32 +0200

    modules done 43/94

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -6,12 +6,15 @@ mod delicious_snacks {
     // use self::fruits::PEAR as ???;
     // use self::veggies::CUCUMBER as ???;
 
-    mod fruits {
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
+
+    pub mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
